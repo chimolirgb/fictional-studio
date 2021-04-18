bab9e7aeec08d426350f4e59b8adf10b108d724e
chimolirgb <chimolilucy@gmail.com>
2021-04-18 13:59:46 +0300

    modified:css/style.css,index.html,js/script.js

---
+++ js/script.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+ $("#img1").click(function(){
+     $("#d").show();
+     $("#img1").hide();  
+ });
+ $("#d").click(function(){
+    $("#img1").show();
+    $("#d").hide();
+});
+
+$("#img2").click(function(){
+    $("#D").show();
+    $("#img2").hide(); 
+});
+$("#D").click(function(){
+    $("#img2").show();
+    $("#D").hide(); 
+
+});
+$("#img3").click(function(){
+    $("#r").show();
+    $("#img3").hide();  
+});
+$("#r").click(function(){
+   $("#img3").show();
+   $("#r").hide();
+});
+});
