089121b877a92ef0ff02cce77b3ddcec43813fa0
chimolirgb <chimolilucy@gmail.com>
2021-04-18 17:50:33 +0300

    modified:css/style.css,index.html,js/script.ja

--- js/script.js
+++ js/script.js
@@ -26,3 +26,38 @@ $("#r").click(function(){
    $("#r").hide();
 });
 });
+
+$(document).ready(function(){
+$("#hover1").hover(function(){
+$("#imgtext1").toggle();
+});
+
+$("#hover2").hover(function(){
+    $("#imgtext2").toggle();
+    });
+
+    $("#hover3").hover(function(){
+        $("#imgtext3").toggle();
+        }); 
+
+        $("#hover4").hover(function(){
+            $("#imgtext4").toggle();
+            }); 
+
+            $("#hover5").hover(function(){
+                $("#imgtext5").toggle();
+                }); 
+                
+                $("#hover6").hover(function(){
+                    $("#imgtext6").toggle();
+                    });  
+                    
+                    $("#hover7").hover(function(){
+                        $("#imgtext7").toggle();
+                        }) 
+                        
+     
+                        $("#hover8").hover(function(){
+                            $("#imgtext8").toggle();
+                            })                     
+});
